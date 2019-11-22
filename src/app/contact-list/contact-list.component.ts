62855f8fe11fecf1db051c943c15da243c32c32d
Andre Steudel <andre.steudel@yellowmap.de>
2019-11-22 16:12:41 +0100

    load contacts from service

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-contact-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  contacts$ = this.contactService.getContactList();
+
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
   }
