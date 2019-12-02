05cdfb6065f5353842d896ecda4c61ab2d1a90eb
Alexandra Gäbele <alexandra.gaebele@cas.de>
2019-12-02 17:17:22 +0100

    Unit tests for ContactService

--- src/app/services/contact.service.spec.ts
+++ src/app/services/contact.service.spec.ts
@@ -1,15 +1,55 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ContactService } from './contact.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 describe('ContactService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ]
-  }));
+  let service: ContactService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
-  it('should be created', () => {
-    const service: ContactService = TestBed.get(ContactService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new ContactService(httpClientSpy);
+  });
+
+  describe('getContactList', () => {
+    it('should send a get request to /contact/list', () => {
+      service.getContactList(null);
+
+      expect(httpClientSpy.get).toHaveBeenCalledWith(environment.api + '/contact/list', jasmine.any(Object));
+    });
+
+    it('should send the search term in the http params', () => {
+      const searchTerm = 'xyz';
+
+      service.getContactList(searchTerm);
+
+      expect(httpClientSpy.get).toHaveBeenCalledWith(jasmine.any(String), { params: { term: searchTerm } });
+    });
+
+    it('should send the search term in the http params even when null', () => {
+      service.getContactList(null);
+
+      expect(httpClientSpy.get).toHaveBeenCalledWith(environment.api + '/contact/list', { params: { term: null } });
+    });
+  });
+
+  describe('getContact', () => {
+    it('should send a get request to /contact/123 when user id is "123"', () => {
+      const userId = '123';
+
+      service.getContact(userId);
+
+      expect(httpClientSpy.get).toHaveBeenCalledWith(environment.api + '/contact/' + userId);
+    });
+  });
+
+  describe('getContactInterests', () => {
+    it('should send a get request to /contact/123/interests when user id is "123"', () => {
+      const userId = '123';
+
+      service.getContactInterests(userId);
+
+      expect(httpClientSpy.get).toHaveBeenCalledWith(environment.api + '/contact/' + userId + '/interests');
+    });
   });
 });
