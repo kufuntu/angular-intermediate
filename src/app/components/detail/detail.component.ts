11a505aa2894ca76335464827daa9b6779d2a0c0
Andre Steudel <andre.steudel@yellowmap.de>
2022-01-20 13:22:08 +0100

    solution

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,38 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { mergeMap, map } from 'rxjs/operators';
+import { combineLatest, Observable, of } from 'rxjs';
+import { map, mergeMap, switchMap, withLatestFrom } from 'rxjs/operators';
 import { ContactService } from '../../services/contact.service';
-import { User } from '../../types/user';
-
+import { User } from "../../types/user";
 
 @Component({
-  selector: 'app-detail',
-  templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.scss']
+  selector: "app-detail",
+  templateUrl: "./detail.component.html",
+  styleUrls: ["./detail.component.scss"],
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent {
+  
+  user$: Observable<User> = this.activatedRoute.params.pipe(
+    map(params => params.id),
+    switchMap(id => this.contactService.getContact(id)),
+    switchMap(user => this.contactService.getContactInterests(user.id).pipe(
+      map(interests => {
+        return { ...user, interests };
+      })
+    ))
+  );
 
-  user: User;
+  user2$: Observable<User> = this.activatedRoute.params.pipe(
+    map(params => params.id),
+    switchMap(id => this.contactService.getContact(id).pipe(
+      withLatestFrom(this.contactService.getContactInterests(id))
+    )),
+    map(([user, interests]) => {
+      return { ...user, interests };
+    })
+  );
+
+  user3$: Observable<User> = this.activatedRoute.params.pipe(
+    map(params => params.id),
+    switchMap(id => combineLatest([
+      this.contactService.getContact(id),
+      this.contactService.getContactInterests(id)
+    ])),
+    map(([user, interests]) => {
+      return { ...user, interests };
+    })
+  );
 
   constructor(
-    private contactService: ContactService,
-    private activeRoute: ActivatedRoute
-  ) { }
+    private activatedRoute: ActivatedRoute,
+    private contactService: ContactService
+    ) {
 
-  ngOnInit() {
-    this.getUser().subscribe(user => {
-      this.user = user;
-    });
-  }
-
-  getUser() {
-    return this.activeRoute.params.pipe(
-      mergeMap(route => this.contactService.getContact(route.id)),
-      mergeMap(user => this.contactService.getContactInterests(user.id).pipe(
-        map(interests => ({ ...user, interests }))
-      ))
-    );
-  }
-
+    }
 }
